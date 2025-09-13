5e85b697aa1c41460050066d9654e2a4e703f57c
mohammedayaz24 <147026092+mohammedayaz24@users.noreply.github.com>
2025-09-13 17:02:34 +0000

    conersion

---
+++ conversionoperation.js
@@ -0,0 +1,10 @@
+let score="ayaz";//string
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let mark= 33aa //NaN => Not a Number;
+console.log(mark);
+console.log(typeof mark);
+
+// "33aa"=>NaN
