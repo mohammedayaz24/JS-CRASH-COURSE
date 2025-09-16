4124a795459c5bb7a00273c56ea31e09ad0fb2f1
mohammedayaz24 <147026092+mohammedayaz24@users.noreply.github.com>
2025-09-16 15:46:03 +0000

    conversion of datatype

--- conversionoperation.js
+++ conversionoperation.js
@@ -1,10 +1,33 @@
-let score="ayaz";//string
+let score="33aaa";//number
 
 console.log(typeof score);
 console.log(typeof(score));
 
-let mark= 33aa //NaN => Not a Number;
-console.log(mark);
-console.log(typeof mark);
+
+let valueInNumber=Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
 
 // "33aa"=>NaN
+//we have to be careful while converting a string to a number
+
+//null=>0
+//undefined=>NaN
+//boolean=>true=>1,
+// false=>0
+//string=>"33"=>33
+
+let IsloggedIn="";
+
+let booleanisLoggedIn=Boolean(IsloggedIn);
+console.log(booleanisLoggedIn);
+
+//1=>true
+//0=>false
+//"hi"=true
+//""=>false
+
+let someNumber=33;
+let stringNumber=String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
