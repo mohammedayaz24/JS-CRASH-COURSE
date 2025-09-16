65daf6b3db22de631c13131c5731778702b9ef7a
mohammedayaz24 <147026092+mohammedayaz24@users.noreply.github.com>
2025-09-16 17:40:48 +0000

    changes

--- conversionoperation.js
+++ conversionoperation.js
@@ -31,3 +31,5 @@ let someNumber=33;
 let stringNumber=String(someNumber);
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+
---
+++ operation.js
@@ -0,0 +1,26 @@
+let value=3;
+let negvalue= -value;
+console.log(negvalue);
+
+console.log(2 + 3);
+console.log(2 - 3);
+console.log(2 * 3);
+console.log(10 / 2);
+console.log(10 % 3);
+
+//contatination
+let str1="hello";
+let str2="  world";
+let str3=str1 + str2;
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 3);
+console.log(1 + 2 + "3");
+
+//it uses toPrimitive for operands
+
+let gameCounter=100;
+++gameCounter;
+console.log(gameCounter);
