348d9e09cc19b968ce2ddb1c8aab2f757d5d2fff
mohammedayaz24 <147026092+mohammedayaz24@users.noreply.github.com>
2025-09-17 13:51:44 +0000

    comparison operator

--- operation.js
+++ operation.js
@@ -7,6 +7,8 @@ console.log(2 - 3);
 console.log(2 * 3);
 console.log(10 / 2);
 console.log(10 % 3);
+console.log("-------------------")
+
 
 //contatination
 let str1="hello";
@@ -24,3 +26,35 @@ console.log(1 + 2 + "3");
 let gameCounter=100;
 ++gameCounter;
 console.log(gameCounter);
+console.log("-------------------")
+
+
+//comaparison
+
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 <= 1);
+console.log(2 == 1);
+console.log(2 != 1);
+console.log("-------------------")
+
+
+console.log("2" > 1);
+console.log("01" == 1);
+console.log("-------------------")
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+//null is only equal to undefined and not to any other value
+console.log("-------------------")
+console.log(undefined > 0);
+console.log(undefined < 0);
+console.log(undefined == 0);
+//undefined is only equal to null and not to any other value
+console.log("-------------------")
+
+//strict equality
+console.log(0 === false);
+console.log(0 == false);
