27af30d6fd3beac39542a8aaf99f42e6bc75f38a
mohammedayaz24 <147026092+mohammedayaz24@users.noreply.github.com>
2025-10-02 14:09:43 +0000

    string lessons

--- string.js
+++ string.js
@@ -3,7 +3,9 @@ const repoCount=50;
 
 console.log(name +repoCount+ "Value");
 
-console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+//string interpolation
+//requires backticks
+console.log(`Hello my name is ${name.toUpperCase()} and my repo count is ${repoCount}`);
 
 const gamename= new String("Ayaz");
 
@@ -12,8 +14,42 @@ console.log(gamename[1]);
 //methods
 console.log(gamename.__proto__);
 
-console.log(gamename.length);
+console.log(gamename.length);//length
 console.log(gamename.toUpperCase());//touppererCase
 console.log(gamename.toLowerCase());//tolowerCase
-console.log(gamename.charAt(2));
-console.log(gamename.indexOf("y"));
+console.log(gamename.charAt(2));//to get character at index
+console.log(gamename.indexOf("y"));//to get index of character
+
+//substring
+console.log("Substring method");
+const newString= gamename.substring(0,3);
+console.log(newString);
+
+//slice
+console.log("Slice method");
+const gamename1="abcdefgh";
+const newString2= gamename1.slice(-8,-4);
+console.log(newString2);
+
+//trim
+console.log("Trim method");
+const gamename2="     Ayaz     ";
+console.log(gamename2);
+console.log(gamename2.trim());
+console.log(gamename2.trimStart());
+console.log(gamename2.trimEnd());
+
+//replace
+console.log("Replace method");
+const url="www.ayaz.com";
+console.log(url.replace("com","in"));
+
+//includes
+console.log("Includes method");
+const gamename3="Ayaz";
+console.log(gamename3.includes("y"));
+
+//split
+console.log("Split method");
+const gamename4="Ayaz-Ahmed-Khan";
+console.log(gamename4.split("-"));
